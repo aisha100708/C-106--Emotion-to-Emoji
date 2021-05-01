1db5563c961d56b744f675cd00ede0e9da6a9fac
aisha100708 <71584539+aisha100708@users.noreply.github.com>
2021-05-01 18:41:06 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-prediction_1 = "";
-prediction_2 = "";
 Webcam.set({
     height: 300,
     width: 350,
@@ -18,6 +16,8 @@ classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models
 function model_loaded() {
     console.log("Model Loaded!");
 }
+prediction_1 = "";
+prediction_2 = "";
 function speak() {
     var synth = window.speechSynthesis;
     prediction_1 = "The 1st prediction is " + prediction_1;
@@ -25,4 +25,39 @@ function speak() {
     var utterance = new SpeechSynthesisUtterance(prediction_1 + prediction_2);
     synth.speak(utterance);
 }
-speak();
+//speak();
+function identify() {
+    new_image = document.getElementById("new_image");
+    classifier.classify(new_image, getResult);
+}
+function getResult(error, result) {
+    if (error) {
+        console.error(error);
+    }
+    else {
+        console.log(result);
+        prediction_1 = result[0].label;
+        prediction_2 = result[1].label;
+        document.getElementById("emotion_name1").innerHTML = prediction_1;
+        document.getElementById("emotion_name2").innerHTML = prediction_2;
+        if (prediction_1 == "Happy") {
+            document.getElementById("emotion_symbol1").innerHTML = "&#128512;";
+        }
+        else if (prediction_1 == "Sad") {
+            document.getElementById("emotion_symbol1").innerHTML = "&#128532;";
+        }
+        else if (prediction_1 == "Angry") {
+            document.getElementById("emotion_symbol1").innerHTML = "&#128545;";
+        }
+        if (prediction_2 == "Happy") {
+            document.getElementById("emotion_symbol2").innerHTML = "&#128512;";
+        }
+        else if (prediction_2 == "Sad") {
+            document.getElementById("emotion_symbol2").innerHTML = "&#128532;";
+        }
+        else if (prediction_2 == "Angry") {
+            document.getElementById("emotion_symbol2").innerHTML = "&#128545;";
+        }
+        speak();        
+    }
+}
